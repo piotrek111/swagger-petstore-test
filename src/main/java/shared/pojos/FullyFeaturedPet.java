75fdb7ecd78a00f7f7c5b904800fbfb352ac2cd7
Piotr Szczepaniak <rydgier-piotr@yahoo.co.uk>
2023-08-23 18:59:03 +0200

    Adding Skeleton for Add Pet functionality as well as POJO classes for building a pet JSON with all possible data fields.

---
+++ src/main/java/shared/pojos/FullyFeaturedPet.java
@@ -0,0 +1,15 @@
+package shared.pojos;
+
+import javax.swing.text.html.HTML;
+import java.util.List;
+
+public class FullyFeaturedPet {
+
+    private long id;
+    private Category category;
+    private String name;
+    private List<String> photoUrls;
+    private List<Tag> tags;
+    private String status;
+
+}
--- src/test/java/test/functional/AddNewPetTests.java
+++ src/test/java/test/functional/AddNewPetTests.java
@@ -1,6 +1,37 @@
 package test.functional;
 
+import org.testng.annotations.Test;
+import shared.pojos.FullyFeaturedPet;
+import shared.pojos.MinimalPet;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
 public class AddNewPetTests {
 
+    private MinimalPet minimalPet;
+    private FullyFeaturedPet fullyFeaturedPet;
+    private long existingPetId;
+
+    @Test
+    public void addNewMinimalPet() {
+
+    }
+    @Test
+    public void addFullyFeaturedPet() {
+
+    }
+
+
+    @Test
+    public void addPetWithAnExistingId() {
+
+    }
+
+    @Test
+    public void addPetWithValidAndInvalidData() {
+
+    }
 
 }
